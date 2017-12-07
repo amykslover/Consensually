30840cecd046a6afe7568726a85151b3802589d3
amykslover <amykslover@gmail.com>
2017-12-07 14:25:39 -0800

    Changed callbackURL to production

--- config/auth.js
+++ config/auth.js
@@ -3,11 +3,11 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1376574052470030',
         'clientSecret'  : '774037ff78c23a030f3ca83dc1b6bb0f',
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
+        'callbackURL'   : 'https://ancient-badlands-21408.herokuapp.com/auth/facebook/callback',
         'profileFields' : ['emails', 'displayName'] // For requesting permissions from Facebook API
     }
         // 'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
-        // 'callbackURL'   : 'https://ancient-badlands-21408.herokuapp.com/',
+        // 'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
 
     // 'googleAuth' : {
     //     'clientID'      : 'your-secret-clientID-here',
